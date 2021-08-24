083b2debf7285830cf109b169d79430aee2b7ae1
coollpice <coollpice@naver.com>
2021-08-24 22:02:18 +0900

    링크까지

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -57,9 +58,21 @@ public class BasicController {
     }
 
 
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
 
     @Component("helloBean")
-    static class HelloBean{
+    static class HelloBean {
         public String hello(String data) {
             return "Hello " + data;
         }
